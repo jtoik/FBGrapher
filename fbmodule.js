1ca8ce1c7f1b062ad8038e45cb5463d6bbd217db
jtoik <jeretoikkadev@gmail.com>
2020-06-01 14:23:26 +0300

    node server working

---
+++ fbmodule.js
@@ -0,0 +1,20 @@
+const {FB} = require('fb')
+const http = require('http')
+require('dotenv').config()
+
+const run = async () => {
+  FB.options({version: 'v7.0'})
+  FB.setAccessToken(process.env.ACCESS_TOKEN);
+  const data = (await FB.api(
+    'act_25064918/insights?fields=campaign_id,campaign_name,account_currency,cpc,impressions&level=campaign&time_increment=7', 'get'))
+  return data
+}
+
+Promise.resolve(run()).catch(console.log)
+
+const server = http.createServer()
+server.on('request', async (req, res) => {  
+  var fbData = await run()
+  res.end(JSON.stringify(fbData))
+})  
+server.listen(3000)
