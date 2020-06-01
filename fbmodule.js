955652964b9d7c52a9e44b3bc73841464f71a289
jtoik <jeretoikkadev@gmail.com>
2020-06-01 15:52:57 +0300

    work in progress

--- fbmodule.js
+++ fbmodule.js
@@ -1,16 +1,21 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
 const {FB} = require('fb')
 const http = require('http')
+
 require('dotenv').config()
 
 const run = async () => {
   FB.options({version: 'v7.0'})
-  FB.setAccessToken(process.env.ACCESS_TOKEN);
+  FB.setAccessToken(process.env.ACCESS_TOKEN)
   const data = (await FB.api(
     'act_25064918/insights?fields=campaign_id,campaign_name,account_currency,cpc,impressions&level=campaign&time_increment=7', 'get'))
   return data
 }
-
-Promise.resolve(run()).catch(console.log)
 
 const server = http.createServer()
 server.on('request', async (req, res) => {  
--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
+  el: '#app',
+  data () {
+    return {
+      info: null
     }
-})
-
-/* 
-var app2 = new Vue({
-  el: '#app-2',
-  data: {
-    message: 'You loaded this page on ' + new Date().toLocaleString()
+  },
+  mounted () {
+    axios
+      .get('http://localhost:3000')
+      .then(response => (this.info = response))
   }
 })
-
-var app3 = new Vue({
-  el: '#app-3',
-  data: {
-    seen: true
-  }
-})
-
-var app4 = new Vue({
-  el: '#app-4',
-  data: {
-    todos: [
-      { text: 'Learn JavaScript' },
-      { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
-  }
-})
-*/
